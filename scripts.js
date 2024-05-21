7fb935747d83aa05f34e87b45c47ee2a0b551605
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-21 16:33:11 +0100

    added clear button, operator logic, refactored addEvents

--- scripts.js
+++ scripts.js
@@ -5,12 +5,13 @@ let currentDisplayNumber = 0
 
 const resultDisplay = document.getElementById("result").querySelector("p")
 const digits = document.getElementById("digits").querySelectorAll("button")
+const clearButton = document.getElementById("clear")
+const operators = document.getElementById("operators").querySelectorAll("button")
 
 const add = (a,b) => a + b 
 const subtract = (a,b) => a - b
 const multiply = (a,b) => a * b
 const divide = (a,b) => a / b
-
 
 const operate = (a,b,op) => {
     switch(op){
@@ -22,8 +23,9 @@ const operate = (a,b,op) => {
     }
 }
 
-const updateDisplay = (result) => resultDisplay.textContent = result;
-
+const updateDisplay = (result) => 
+    resultDisplay.textContent = result;
+    //currentDisplayNumber = result.toString();
 
 function pressedNumber(event){
     if(currentDisplayNumber == 0){
@@ -32,10 +34,45 @@ function pressedNumber(event){
     else{
         currentDisplayNumber += event.target.textContent
     }
+    if(!operator){
+        firstNumber = currentDisplayNumber
+    }
+    else{
+        splitResult = currentDisplayNumber.split(operator)
+        secondNumber = splitResult[splitResult.length-1]
+    }
     updateDisplay(currentDisplayNumber)
-
 }
 
-for (const button of digits){
-    button.addEventListener("click", pressedNumber)
+function pressedOperator(event){
+    if (!secondNumber){
+        operator = (event.target.textContent)
+        currentDisplayNumber += operator
+        updateDisplay(currentDisplayNumber)
+    }
+    else if(secondNumber){
+        updateDisplay(operate(firstNumber,secondNumber,operator))
+    }
+    else if(operator){
+        updateDisplay(currentDisplayNumber)
+    }
 }
+
+function clearAll(){
+    currentDisplayNumber = 0
+    updateDisplay(currentDisplayNumber)
+}
+
+function addEvents(){
+    for (const button of digits){
+        button.addEventListener("click", pressedNumber)
+    }
+    
+    for (const button of operators){
+        button.addEventListener("click",pressedOperator)
+    }
+
+    clearButton.addEventListener("click",clearAll)
+}
+
+addEvents()
